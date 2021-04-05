768edeee5b1b8885d2013a773db52c8d681feed6
Noah <hanlun.song@gmail.com>
2021-04-05 15:56:58 -0400

    add a modal

--- frontend/src/components/Layout.vue
+++ frontend/src/components/Layout.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Modal from './Modal.vue'
+
+const modalOpen = ref(false)
+</script>
+
 <template>
   <div class="relative h-screen bg-gray-800 overflow-hidden">
     <div class="hidden sm:block sm:absolute sm:inset-0" aria-hidden="true">
@@ -42,14 +49,63 @@
         </div>
         <div class="flex">
           <a
-            href="#"
+            @click="modalOpen = true"
             class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
           >
             Log in
           </a>
         </div>
       </nav>
-      <slot></slot>
+      <slot />
     </div>
   </div>
+
+  <teleport to="body">
+    <Modal v-if="modalOpen"
+      ><div>
+        <div
+          class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
+        >
+          <!-- Heroicon name: outline/check -->
+          <svg
+            class="h-6 w-6 text-green-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 13l4 4L19 7"
+            />
+          </svg>
+        </div>
+        <div class="mt-3 text-center sm:mt-5">
+          <h3
+            class="text-lg leading-6 font-medium text-gray-900"
+            id="modal-title"
+          >
+            Payment successful
+          </h3>
+          <div class="mt-2">
+            <p class="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur amet labore.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="mt-5 sm:mt-6">
+        <button
+          type="button"
+          class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+        >
+          Go back to dashboard
+        </button>
+      </div></Modal
+    >
+  </teleport>
 </template>
---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="fixed z-10 inset-0 overflow-y-auto"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true"
+  >
+    <div
+      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+    >
+      <transition
+        enter-active-class="transition ease-out duration-300"
+        enter-from-class="transform opacity-0"
+        enter-to-class="transform opacity-100"
+        leave-active-class="transition ease-in duration-200"
+        leave-from-class="transform opacity-100"
+        leave-to-class="transform opacity-0"
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        ></div>
+      </transition>
+
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span
+        class="hidden sm:inline-block sm:align-middle sm:h-screen"
+        aria-hidden="true"
+        >&#8203;</span
+      >
+
+      <transition
+        enter-active-class="transition ease-out duration-300"
+        enter-from-class="transform opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        enter-to-class="transform opacity-100 translate-y-0 sm:scale-100"
+        leave-active-class="transition ease-in duration-200"
+        leave-from-class="transform opacity-100 translate-y-0 sm:scale-100"
+        leave-to-class="transform opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      >
+        <div
+          class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+        >
+          <slot />
+        </div>
+      </transition>
+    </div>
+  </div>
+</template>
