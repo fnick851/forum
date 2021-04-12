fc507af0ceb309cb6a310552f117d69a12e3e402
Noah <hanlun.song@gmail.com>
2021-04-12 14:10:31 -0400

    add signup route (createUser service)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common'
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common'
 import { AuthService } from './auth/auth.service'
 import { LocalAuthGuard } from './auth/guards/local-auth.guard'
 import { NoJWT } from './common/decorators/no-jwt.decorator'
+import { CreateUserDto } from './users/dtos/create-user.dto'
+import { UsersService } from './users/users.service'
 
 @Controller('api')
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
 
   @NoJWT()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user)
+  }
+
+  @NoJWT()
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto)
   }
 }
